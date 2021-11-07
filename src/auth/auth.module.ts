5a560683dc7a214d809a5b2c41f9993fb63a5f49
Zharckov <23assasyncred03@gmail.com>
2021-11-08 01:00:14 +0300

    Add .example.env

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { MailModule } from 'src/mail/mail.module';
+import { TokensModule } from 'src/tokens/tokens.module';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [MailModule, UsersModule],
+  imports: [MailModule, UsersModule, TokensModule],
   controllers: [AuthController],
   providers: [AuthService],
 })
